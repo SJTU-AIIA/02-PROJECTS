dcf9575b2b045ca8d8eb114be0298cbbde8ddc08
Qisheng Sun <dysonfabiensun@sjtu.edu.cn>
2025-04-16 22:52:31 +0800

    updated actions

--- .github/plugins/validate_manifest.py
+++ .github/plugins/validate_manifest.py
@@ -12,12 +12,18 @@ schema = {
 }
 
 def validate(manifest_path, author):
-    with open(manifest_path) as f:
-        data = json.load(f)
-        jsonschema.validate(data, schema) # validates format
-    data["author"] = [author]
-    with open(manifest_path, "w") as f:
-        json.dump(data, f, indent=2, sort_keys=True)   # adds author information directly
+    try:
+        with open(manifest_path) as f:
+            data = json.load(f)
+            jsonschema.validate(data, schema) # validates format
+        data["author"] = [author]
+        with open(manifest_path, "w") as f:
+            json.dump(data, f, indent=2, sort_keys=True)   # adds author information directly
+        sys.exit(0)
+    except jsonschema.ValidationError:
+        sys.exit(2)
+    except:
+        sys.exit(1)
 
 if __name__ == "__main__":
     validate(sys.argv[1], sys.argv[2])
