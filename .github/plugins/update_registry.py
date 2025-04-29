be8b017e67bf34333fffc74474c1160bbd5b5cff
Qisheng Sun <dysonfabiensun@sjtu.edu.cn>
2025-04-29 12:49:37 +0800

    Fixed bugs in update_registry.py

--- .github/plugins/update_registry.py
+++ .github/plugins/update_registry.py
@@ -9,7 +9,7 @@ def update_registry():
     utc_8 = timezone(timedelta(hours=8))
 
     registry = {"projects": []}
-    for manifest in glob.glob(os.path.join(str(ROOT_DIR),"projects/**/manifest.json")):
+    for manifest in glob.glob(os.path.join(str(ROOT_DIR),"projects/[!.*]*/manifest.json")):
         with open(manifest) as f:
             data = json.load(f)
             data["path"] = os.path.dirname(manifest)
